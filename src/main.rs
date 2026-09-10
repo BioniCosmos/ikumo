d9af2983065aa39b778d98a1c9787c4b2a7244b6
Hane Velarith <41363844+BioniCosmos@users.noreply.github.com>
2026-09-11 03:58:34 +0800

    Support parsing SSH config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{fs, process::Command};
+use std::{collections::HashMap, fs, process::Command};
 
+use anyhow::bail;
 use serde::Deserialize;
 use tokio::main;
 
@@ -42,5 +43,131 @@ async fn main() {
             );
             break;
         }
+    }
+}
+
+#[derive(Clone, Debug, Default, PartialEq)]
+struct SSHConfig {
+    host_name: String,
+    port: u16,
+    user: String,
+}
+
+fn parse_ssh_config(raw: &str) -> anyhow::Result<HashMap<String, SSHConfig>> {
+    let mut m = HashMap::new();
+
+    let raw = raw.as_bytes();
+    let mut i = 0;
+
+    macro_rules! consume_line {
+        () => {{
+            while i < raw.len() {
+                if raw[i] == b'\n' {
+                    break;
+                }
+                i += 1;
+            }
+            i += 1;
+        }};
+    }
+
+    macro_rules! read_value {
+        ($start:expr) => {{
+            i = $start;
+
+            while i < raw.len() {
+                if raw[i] != b' ' {
+                    break;
+                }
+                i += 1;
+            }
+            let start = i;
+
+            consume_line!();
+
+            &raw[start..i - 1]
+        }};
+    }
+
+    let mut host = "";
+    let mut config = SSHConfig::default();
+
+    while i < raw.len() {
+        match raw[i] as char {
+            ' ' | '\t' | '\n' => i += 1,
+            'H' => {
+                if i + 8 <= raw.len() && &raw[i..i + 8] == b"HostName" {
+                    if host.is_empty() {
+                        bail!("missing `Host`");
+                    }
+                    config.host_name = String::from_utf8(read_value!(i + 8).to_vec())?;
+                } else if i + 4 <= raw.len() && &raw[i..i + 4] == b"Host" {
+                    if !host.is_empty() {
+                        m.insert(host.to_owned(), config.clone());
+                        config = SSHConfig::default();
+                    }
+                    host = str::from_utf8(read_value!(i + 4))?
+                } else {
+                    consume_line!();
+                }
+            }
+            'P' => {
+                if i + 4 <= raw.len() && &raw[i..i + 4] == b"Port" {
+                    if host.is_empty() {
+                        bail!("missing `Host`");
+                    }
+                    config.port = str::from_utf8(read_value!(i + 4))?.parse()?;
+                } else {
+                    consume_line!();
+                }
+            }
+            'U' => {
+                if i + 4 <= raw.len() && &raw[i..i + 4] == b"User" {
+                    if host.is_empty() {
+                        bail!("missing `Host`");
+                    }
+                    config.user = String::from_utf8(read_value!(i + 4).to_vec())?;
+                } else {
+                    consume_line!();
+                }
+            }
+            _ => consume_line!(),
+        }
+    }
+
+    if !host.is_empty() {
+        m.insert(host.to_owned(), config.clone());
+    }
+
+    Ok(m)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_matches;
+
+    use super::*;
+
+    #[test]
+    fn test_parse_ssh_config() {
+        let raw = r#"Host foo
+    HostName example.com
+    Port 2233
+    User me"#;
+        let m = parse_ssh_config(raw);
+        assert_matches!(m, Ok(_));
+
+        let m = m.unwrap();
+        assert_eq!(
+            m,
+            HashMap::from([(
+                "foo".to_owned(),
+                SSHConfig {
+                    host_name: "example.com".to_owned(),
+                    port: 2233,
+                    user: "me".to_owned()
+                }
+            )])
+        );
     }
 }
